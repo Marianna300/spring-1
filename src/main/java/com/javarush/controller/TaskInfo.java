9278ae56083e5e558281c0009df1ff55dc2bc1a0
Marianna300 <marianna-rgs@yandex.ru>
2023-03-22 20:05:05 +0300

    Initial commit

---
+++ src/main/java/com/javarush/controller/TaskInfo.java
@@ -0,0 +1,27 @@
+package com.javarush.controller;
+
+import com.javarush.domain.Status;
+
+public class TaskInfo {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    private Status status;
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+
+
+}
